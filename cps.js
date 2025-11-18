7b1741ba3fed29f1c5e3d89e1a2915e46cd85f88
yezitv <lazy1286@gmail.com>
2025-11-18 20:41:21 +0800

    Update cps.js

--- cps.js
+++ cps.js
@@ -63,14 +63,10 @@ var ArrList1=[
 ];
 
 var ArrList2=[
-    // "https://go5fxhaw3vi.ixkfzkej.cn/51x0904/?code=888133",
-     "https://aopy-c.alsdbjsh.com/articles/1689?page=d3.html&dl=h5&ii=YSJGXP&hmsr=YSJGXP",
-    // "https://www.mm21.net/?qd=yhcxtuav",
-	// "https://dd.esud56.com/download3/3892_0.html",
-	// "https://dd.esud56.com/download2/3892_0.html",
-	"https://dd.esud56.com/download4/3892_0.html",
-	"https://dd.esud56.com/download/3892_0.html",
-  
+  	 "https://aop-c.tuxiaozhuapp.com/articles/1689?page=d3.html&dl=h5&ii=YSJGXP&hmsr=YSJGXP",
+	 "https://aop-c.tuxiaozhuapp.com/articles/1689?page=d2.html&dl=h5&ii=YSJGXP&hmsr=YSJGXP",
+    "https://nyj.vpmouoiyq.cn/page1?c=1013&v=rIInaz",
+	  "https://nyj.vpmouoiyq.cn/page2?c=1013&v=rIInaz",
 
 ];
 var close=["/upload/pds/close.jpeg"];
