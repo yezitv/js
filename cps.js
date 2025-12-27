7bde316c77af74e2b09cb739b9483de81defac0e
yezitv <lazy1286@gmail.com>
2025-12-27 23:29:50 +0800

    Update cps.js

--- cps.js
+++ cps.js
@@ -63,8 +63,8 @@ var ArrList1=[
 ];
 
 var ArrList2=[
-   "https://aopa-c.jzxczpw.com/articles/1689?page=d3.html&dl=h5&ii=YSJGXP&hmsr=YSJGXP",
-	 "https://aopa-c.jzxczpw.com/articles/1689?page=d2.html&dl=h5&ii=YSJGXP&hmsr=YSJGXP",
+   // "https://aopa-c.jzxczpw.com/articles/1689?page=d3.html&dl=h5&ii=YSJGXP&hmsr=YSJGXP",
+	 // "https://aopa-c.jzxczpw.com/articles/1689?page=d2.html&dl=h5&ii=YSJGXP&hmsr=YSJGXP",
    //  "https://nyj.vpmouoiyq.cn/page1?c=1013&v=rIInaz",
 	  // "https://nyj.vpmouoiyq.cn/page2?c=1013&v=rIInaz",
 
